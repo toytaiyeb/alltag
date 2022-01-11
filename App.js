b9edcd7b5182e2144ef0aa02d9d337f6f43ac181
Taiyeb Radiowala <toy24052003@gmail.com>
2022-01-11 23:14:09 +0530

    Update App.js

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'blue',
+    backgroundColor: 'purple',
     alignItems: 'center',
     justifyContent: 'center',
   },
